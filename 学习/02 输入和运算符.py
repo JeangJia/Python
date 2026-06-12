ec8e00b2ec7dcab8f5bea4215d02f7d5fb485b28
Jeang <2609231566@qq.com>
2026-06-12 12:02:44 +0800

    输入和运算符

---
+++ 学习/02 输入和运算符.py
@@ -0,0 +1,15 @@
+num = int(input("num:"))
+print(f"num:{num}")
+
+# 取模
+print(6 % 4)
+# 整除
+print(6 // 4)
+# 幂
+print(2 ** 3)
+# 优先级: ** -> * -> /,//->+,-
+
+print(1<=2 and 2>=3) # False
+print(1<=2 or 2>=3) # True
+print(not 0) # True
+
